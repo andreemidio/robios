be839643e768bb2bcfa23aac9c76268d40db7a6b
Andre Luiz Emidio Junior <andre.junior@engdb.com.br>
2021-12-28 11:02:48 -0300

    Correção do dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,3 +7,7 @@ python manage.py collectstatic --noinput
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
+
+# Start server
+echo "Starting server"
+gunicorn --worker-class=gevent --workers=3 --worker-connections=1000 config.wsgi:application --bind=0.0.0.0:8000 --log-level=DEBUG
