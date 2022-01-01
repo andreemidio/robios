17d82f299fcb4dabd57f21606473ed7111ddaa6d
Andre Luiz Emidio Junior <andre.junior@engdb.com.br>
2022-01-01 20:39:57 -0300

    Inclusão do APM

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'elasticapm.contrib.django.middleware.TracingMiddleware',git
+    'elasticapm.contrib.django.middleware.TracingMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
