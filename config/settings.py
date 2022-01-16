eef3a8b7e7813bdbdcab54e7a4d490b65430a68f
Andre Luiz Emidio Junior <andre.junior@engdb.com.br>
2022-01-16 20:25:42 -0300

    Correção do env

--- config/settings.py
+++ config/settings.py
@@ -239,6 +239,10 @@ CELERY_BROKER_URL = "redis://localhost:6379/"
 # CELERY_RESULT_BACKEND = "redis://redis:6379"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/"
 
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
 # CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 # CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 
